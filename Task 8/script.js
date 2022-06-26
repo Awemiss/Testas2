e0ac5ca7ba09eca1d6632a825aee0e15a7cdee51
Awemiss <kazutsu2@gmail.com>
2022-06-26 23:36:41 +0300

    Task7 ir Taks8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,35 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+
+    this.read = function() {
+      this.a = +prompt('a ', 0);
+      this.b = +prompt('b ', 0);
+    };
+  
+    this.sum = function() {
+      return this.a + this.b;
+    };
+
+    this.sub = function() {
+        return this.a - this.b;
+    };
+  
+    this.mul = function() {
+      return this.a * this.b;
+    };
+
+    this.divi = function() {
+        return this.a / this.b;
+    };
+  }
+  
+  let calculator = new Calculator();
+  calculator.read();
+  
+  alert('Summation = ' + calculator.sum());
+  alert('Subtraction = ' + calculator.sub());
+  alert('Multiplication = ' + calculator.mul());
+  alert('Division = ' + calculator.divi().toFixed(2));
